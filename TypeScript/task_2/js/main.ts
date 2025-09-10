7985cd6a248c278491cf592f36ef684d895e2300
Roullito <jmoleins@laposte.net>
2025-09-10 12:47:48 +0200

    Add functions to determine employee type and execute tasks based on role

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -50,3 +50,36 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  if (employee instanceof Director) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  else {
+    return employee.workTeacherTasks();
+  }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  return "Teaching History";
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
